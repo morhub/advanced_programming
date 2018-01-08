1275df37dfae671e1d89c9039c3cee0b6d54388a
Mor Huberman <morhub58@gmail.com>
2018-01-08 23:35:43 +0200

    solved to problem - joker works!

--- templates_sln/DataMap.h
+++ templates_sln/DataMap.h
@@ -10,6 +10,7 @@ template<int Order, int K>
 class DataMap
 {
 	map<int, DataMap<Order - 1, K>> _map;
+	static const int joker = std::numeric_limits<int>::min();
 
 public:
 	void insert(Puzzle2dPiece<K>* p) {
@@ -23,30 +24,13 @@ public:
 
 		//recursively insert to lower level
 		_map[coor].insert(p);
+		_map[joker].insert(p);
 	}
 
 	list<Puzzle2dPiece<K>*> get(Puzzle2dPiece<K>* p) {
 		int dim =  p->getDimension();
 		int coor = p->getCoorAt(dim - Order);
 		
-		if (coor == std::numeric_limits<int>::min())
-		{
-			list<Puzzle2dPiece<K>*> res;
-			for (int i = -K; i <= K; i++)
-			{
-				if (_map.find(i) != _map.end()) //element found
-					res.splice(res.end(), _map[i].get(p)); //append lists in the right order
-				cout << "DataMap: " << endl;
-				for (auto& part : res)
-				{
-					cout << *part << endl;
-
-				}
-			}
-
-			return res;
-		}
-
 		if (_map.find(coor) != _map.end()) //element found
 			return _map[coor].get(p);
 
@@ -58,6 +42,7 @@ template<int K>
 class DataMap<1, K>
 {
 	map<int, list<Puzzle2dPiece<K>*>> _map;
+	static const int joker = std::numeric_limits<int>::min();
 
 public:
 	void insert(Puzzle2dPiece<K>* p) {
@@ -71,6 +56,7 @@ public:
 
 		//actually insert part to list
 		_map[coor].push_back(p);
+		_map[joker].push_back(p);
 	}
 
 	 list<Puzzle2dPiece<K>*> get(Puzzle2dPiece<K>* p) {
